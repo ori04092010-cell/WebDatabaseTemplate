bbd44635932c981148170cd6773e409a09d1934e
_ <_>
2026-04-29 10:54:17 +0300

    changed buttons ids

--- website/pages/index.ts
+++ website/pages/index.ts
@@ -1,21 +1,10 @@
-import type { Item } from "types";
-import { send } from "clientUtilities";
-import { create } from "componentUtilities";
+const goSignUp = document.getElementById("goSignUp") as HTMLButtonElement;
+const goLogIn = document.getElementById("goLogIn") as HTMLButtonElement;
 
-var itemInput = document.querySelector<HTMLInputElement>("#itemInput")!;
-var amountInput = document.querySelector<HTMLInputElement>("#amountInput")!;
-var addButton = document.querySelector<HTMLButtonElement>("#addButton")!;
-var itemsUl = document.querySelector<HTMLUListElement>("#itemsUl")!;
+goSignUp.addEventListener("click", () => {
+  window.location.href = "signup.html";
+});
 
-var items = await send<Item[]>("getItems");
-
-for (var i = 0; i < items.length; i++) {
-  var itemLi = create("li");
-  itemLi.innerText = `${items[i].amount} ${items[i].name}`;
-  itemsUl.append(itemLi);
-}
-
-addButton.onclick = async function() {
-  await send("addItem", itemInput.value, parseInt(amountInput.value));
-  location.reload();
-};
+goLogIn.addEventListener("click", () => {
+  window.location.href = "login.html";
+});
